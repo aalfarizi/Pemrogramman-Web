f90f90376991645173e8df7488821a28a2885ce3
Alfarizzi Ramaputra <alfar@Gray.local>
2026-04-08 11:19:22 +0700

    matikan timeslot

--- main.js
+++ main.js
@@ -20,7 +20,9 @@ const times = [
   '06:00','07:00','08:00','09:00','10:00','11:00','12:00','13:00',
   '14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00'
 ];
-const unavailable = ['09:00', '14:00', '18:00'];
+
+// Nah ini yang diubah, dikosongin biar semua jam available!
+const unavailable = []; 
 let selectedTime = '';
 
 function renderSlots() {
